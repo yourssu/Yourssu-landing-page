523b880d19a82b217e3e86283023e950830ff8e7
Minha Ahn <dks4857@gmail.com>
2023-01-30 17:23:59 +0900

    first commit

---
+++ gatsby-config.ts
@@ -0,0 +1,38 @@
+import { GatsbyConfig } from 'gatsby';
+
+const path = require('path');
+// Get paths of Gatsby's required rules, which as of writing is located at:
+// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
+// gatsby/src/utils/eslint-rules
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  'node_modules',
+  'gatsby',
+  'dist',
+  'utils',
+  'eslint-rules',
+);
+
+const config: GatsbyConfig = {
+  // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
+  // don't need to define it here (just if you need to change the options)
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be omitted or customized
+        stages: ['develop'],
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        exclude: ['node_modules', 'bower_components', '.cache', 'public'],
+        // Any additional eslint-webpack-plugin options below
+        // ...
+      },
+    },
+  ],
+  jsxRuntime: `automatic`,
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Title from '../components/Title';
+
+export default function Home() {
+  return (
+    <main>
+      <Title>dfdfdf</Title>
+      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
+      <p>
+        Follow me on Twitter (
+        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
+      </p>
+    </main>
+  );
+}

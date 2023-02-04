22d93687cc752be904f5348a603b06ada55bd993
Minha Ahn <dks4857@gmail.com>
2023-02-04 23:13:42 +0900

    추가 - 부서별 섹션, 인재상 섹션

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
-import Title from '../components/Title';
+import Banner from '@/container/home/banner';
+import Team from '@/container/home/Team';
+import Project from '@/container/home/project';
+import Ideal from '@/container/home/Ideal';
+import Layout from '../components/Layout';
+import Seo from '@/components/Seo';
 
 export default function Home() {
   return (
-    <main>
-      <Title>dfdfdf</Title>
-      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-      <p>
-        Follow me on Twitter (
-        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-      </p>
-    </main>
+    <Layout pageTypes="home">
+      <Banner />
+      <Team />
+      <Ideal />
+      <Project />
+    </Layout>
   );
+}
+
+export function Head() {
+  return <Seo title="home" />;
 }

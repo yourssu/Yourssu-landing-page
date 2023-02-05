a6ebc9e22084ad89ad25c98fc12217e66a4eb03f
Minha Ahn <dks4857@gmail.com>
2023-02-05 14:00:19 +0900

    수정 - TeamButton css 수정

--- src/container/home/Team.tsx
+++ src/container/home/Team.tsx
@@ -31,14 +31,14 @@ function Team() {
   `);
   const data = teamdata.allFile.edges;
   return (
-    <div className="py-20 min-w-max">
+    <div className="flex flex-col items-center py-20">
       <SectionIntro
         title="TEAM YOURSSU"
         description="안녕하세요. 숭실인을 위한. 숭실인에 의해 당신의 숭실을 만들어가는 동아리 유어슈입니다.<br />숭실대 학생들의 즐거운 캠퍼스 라이프를 위한 서비스를 만들고 있습니다."
         image={lineImg}
         color="text-Text_Color1-0"
       />
-      <div className="flex flex-row justify-center items-center">
+      <div className="flex flex-row flex-wrap justify-center">
         {data.map((data1: TeamButtonData) => (
           <TeamButton
             key={data1.node.publicURL}

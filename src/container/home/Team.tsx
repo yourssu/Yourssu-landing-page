c6fb7797088bc85d4340b97f9f979f05d04f7a82
Minha Ahn <dks4857@gmail.com>
2023-02-07 20:03:50 +0900

    수정 - 간격 재조절, png를 svg로 변경, 간단한 이미지는 css로 변경

--- src/container/home/Team.tsx
+++ src/container/home/Team.tsx
@@ -1,17 +1,14 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import TeamButton from '@/components/TeamButton';
 import SectionIntro from '@/components/SectionIntro';
-import lineImg from '../../images/teams/Line.png';
 
 type TeamButtonData = {
-  node: {
-    publicURL: string;
-    name: string;
-  };
+  publicURL: string;
+  name: string;
 };
 
 function Team() {
-  const teamdata = useStaticQuery(graphql`
+  /* const teamdata = useStaticQuery(graphql`
     query {
       allFile(
         filter: {
@@ -28,23 +25,39 @@ function Team() {
         }
       }
     }
+  `); */
+  /* const teamData = useStaticQuery(graphql`
+    query {
+      team: allFile( filter: { sourceInstanceName: { eq: "teams } } ) {
+        nodes {
+          publicURL
+          name
+        }
+      }
+    }
+  `); */
+  const imgData = useStaticQuery(graphql`
+    query {
+      teams: allFile(filter: { sourceInstanceName: { eq: "teams" } }) {
+        nodes {
+          publicURL
+          name
+        }
+      }
+    }
   `);
-  const data = teamdata.allFile.edges;
+  const teamData = imgData.teams.nodes;
   return (
-    <div className="flex flex-col items-center py-20">
+    <div className="flex flex-col items-center py-20 h-[630px]">
       <SectionIntro
         title="TEAM YOURSSU"
         description="안녕하세요. 숭실인을 위한. 숭실인에 의해 당신의 숭실을 만들어가는 동아리 유어슈입니다.<br />숭실대 학생들의 즐거운 캠퍼스 라이프를 위한 서비스를 만들고 있습니다."
-        image={lineImg}
+        divStyle="bg-gradient-to-t from-gradient_color1-0 to-gradient_color2-0 w-[2px] h-[39px]"
         color="text-Text_Color1-0"
       />
       <div className="flex flex-row flex-wrap justify-center">
-        {data.map((data1: TeamButtonData) => (
-          <TeamButton
-            key={data1.node.publicURL}
-            img={data1.node.publicURL}
-            name={data1.node.name}
-          />
+        {teamData.map((data: TeamButtonData) => (
+          <TeamButton key={data.name} img={data.publicURL} name={data.name} />
         ))}
       </div>
     </div>

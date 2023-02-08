c832e0740f77ba11a54cee599111695ac3578fec
Minha Ahn <dks4857@gmail.com>
2023-02-08 19:23:03 +0900

    수정 - 반응형 적용

--- src/components/SectionIntro.tsx
+++ src/components/SectionIntro.tsx
@@ -7,13 +7,13 @@ type IntroProps = {
 
 function SectionIntro({ title, description, divStyle, color }: IntroProps) {
   return (
-    <div className="mb-[52px] flex flex-col items-center">
+    <div className="mb-[52px] flex flex-col items-center mx-[50px]">
       <div className={divStyle} />
       <h1 className="mt-5 mb-2 font-Roboto font-medium text-[36px]">{title}</h1>
       {description.split('<br />').map((des) => (
         <p
           key={des}
-          className={`flex text-center font-NeoR text-[22px] leading-relaxed tracking-tighter ${color}`}
+          className={`flex text-center font-NeoR text-[22px] leading-relaxed ${color}`}
         >
           {des}
           <br />

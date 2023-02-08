bb8ad7d20f7f296d158afd5b7c75334033f8a8f1
george <hyunrim03@naver.com>
2023-02-08 19:27:17 +0900

    update_setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,13 @@ module.exports = {
         Text_Color2: ["#525252"],
       }
     },
+    screens: {
+      sm: {"min": "0px", 'max': "580px"},
+      md: {"min": "580px", 'max': "869px"},
+      lg: {"min": "870px", 'max': "1159px"},
+      xl: {"min": "1160px", 'max': "1440px"},
+      xxl: {"min": "1441px"}
+    }
   },
   plugins: [],
 }

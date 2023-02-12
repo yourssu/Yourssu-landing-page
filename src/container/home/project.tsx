4d0f17b48fbc2cb3876e65b25ab2affbef21c90c
Minha Ahn <dks4857@gmail.com>
2023-02-12 14:49:49 +0900

    프로젝트 섹션 intro 설정 및 마진 설정

--- src/container/home/project.tsx
+++ src/container/home/project.tsx
@@ -1,19 +1,32 @@
+import { useMediaQuery } from 'react-responsive';
 import Carousel from '@/components/Carousel';
+import SectionIntro from '@/components/SectionIntro';
 import useCarouselDetail from '@/hooks/projects/hook';
 
 function Project() {
+  const windowSize = useMediaQuery({
+    query: '(min-width: 1081px)',
+  });
   const { data } = useCarouselDetail();
+
   return (
-    <div className="">
-      <div className="flex flex-col justify-center items-center w-full">
-        <div className=" bg-black h-[39px] w-[2px] " />
-        <h1 className=" text-2xl font-medium mt-4 mb-2 text-black">Project</h1>
-        <p className=" text-center text-gray-500 text-sm mt-3 leading-relaxed -tracking-tighter">
-          유어슈는 다양한 tf팀들의 프로젝트를 통해 숨쉴때 성적표, 숨쉴때
-          커뮤니티, 숨실위키,
-          <br /> 숨쉴때 뭐먹지, 뿌슝이 테스트, 점뿌 등의 서비스를 만들어가가고
-          있어요.
-        </p>
+    <div className="xs:mb-[100px] sm:mb-[200px] md:mb-[300px] mb-[350px]">
+      <div className="xs:pt-[50px] sm:pt-[60px] pt-[80px]">
+        {windowSize ? (
+          <SectionIntro
+            title="Project"
+            description="유어슈는 다양한 tf팀들의 프로젝트를 통해 숨쉴때 성적표, 숨쉴때 커뮤니티, 숨실위키,<br />숨쉴때 뭐먹지, 뿌슝이 테스트, 점뿌 등의 서비스를 만들어가고 있어요."
+            divStyle="bg-black"
+            color="text-Text_Color2-0"
+          />
+        ) : (
+          <SectionIntro
+            title="Project"
+            description="유어슈는 다양한 tf팀들의 프로젝트를 통해 숨쉴때 성적표, 숨쉴때 커뮤니티, 숨실위키, 숨쉴때 뭐먹지, 뿌슝이 테스트, 점뿌 등의 서비스를 만들어가고 있어요."
+            divStyle="bg-black"
+            color="text-Text_Color2-0"
+          />
+        )}
       </div>
       <Carousel itemsData={data.projectImgData.nodes} />
     </div>

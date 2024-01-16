d9eb3f851765351ac6179452eeb9608930b7087a
aaminha <dks4857@gmail.com>
2024-01-16 19:29:12 +0900

    design: 로드투프로 컴포넌트 헤더 작성

--- src/containers/description/RoadToPro/index.tsx
+++ src/containers/description/RoadToPro/index.tsx
@@ -1,5 +1,33 @@
+import tw from 'tailwind-styled-components';
+
+const dummy = '유어슈와 리걸팀의 이야기';
+
 function RoadToPro() {
-  return <div>road to pro</div>;
+  return (
+    <section>
+      <TitleContainer>
+        <Title>Road to Pro</Title>
+        <SubTitle>{dummy}</SubTitle>
+      </TitleContainer>
+      {/* TODO: 영상 넣기 */}
+    </section>
+  );
 }
 
 export default RoadToPro;
+
+const TitleContainer = tw.div`
+  flex
+  flex-col
+  gap-2
+`;
+
+const Title = tw.div`
+  h3
+  text-black-0
+`;
+
+const SubTitle = tw.div`
+  body3
+  text-gray1-0
+`;

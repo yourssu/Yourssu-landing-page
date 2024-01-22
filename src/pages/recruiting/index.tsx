dc6f146aa14e1cbe76c7236ab197b9049404205c
george <hyunrim03@naver.com>
2024-01-22 20:07:54 +0900

    design : 1920 화면 컴포넌트 제작

--- src/pages/recruiting/index.tsx
+++ src/pages/recruiting/index.tsx
@@ -3,13 +3,19 @@ import Seo from '@/components/Seo';
 import About from '@/containers/select/About';
 import ApplyProcedure from '@/containers/select/ApplyProcedure';
 import Banner from '@/containers/select/Banner';
+import FAQ from '@/containers/select/FAQ';
+import Ideal from '@/containers/select/Ideal';
+import Supporting from '@/containers/select/Supporting';
 
 function Recruiting() {
   return (
-    <Container className="flex flex-col items-center">
+    <Container className="flex flex-col items-center gap-[150px]">
       <Banner />
+      <Ideal />
       <About />
       <ApplyProcedure />
+      <Supporting />
+      <FAQ />
     </Container>
   );
 }

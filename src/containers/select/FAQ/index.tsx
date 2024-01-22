c0002b900e24a2c0960e1a83a56ab4bc221d0d87
george <hyunrim03@naver.com>
2024-01-22 20:07:17 +0900

    design: FAQ 컴포넌트 제작

--- src/containers/select/FAQ/index.tsx
+++ src/containers/select/FAQ/index.tsx
@@ -1,5 +1,32 @@
+import tw from 'tailwind-styled-components';
+import QuestionCard from './QuestionCard';
+import useFAQDetail from './hook';
+
 function FAQ() {
-  return <div>유어슈 about 컨테이너</div>;
+  const { faq, imgData } = useFAQDetail();
+
+  return (
+    <Container className="flex flex-col items-center">
+      <span className="font-Pretendard text-[50px] font-[600] leading-[59.67px] tracking-[-1%]">
+        자주 묻는 질문
+      </span>
+      <div className="flex flex-col items-center gap-[20px]">
+        {faq.map((value) => {
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <QuestionCard
+              question={value.question}
+              smallArrow={imgData.smallarrow.nodes[0]}
+            />
+          );
+        })}
+      </div>
+    </Container>
+  );
 }
+
+const Container = tw.div`
+  gap-[60px]
+`;
 
 export default FAQ;

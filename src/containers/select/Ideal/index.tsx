259217c9c24ec02396d2c217ee3538d4acfec949
george <hyunrim03@naver.com>
2024-01-22 20:06:53 +0900

    design: Ideal 컴포넌트 제작

--- src/containers/select/Ideal/index.tsx
+++ src/containers/select/Ideal/index.tsx
@@ -1,5 +1,28 @@
+import { useRef } from 'react';
+import useIdealDetail from './hook';
+
 function Ideal() {
-  return <div>유어슈 Ideal 컨테이너</div>;
+  const { imgData } = useIdealDetail();
+  const ideal = ['자발적인', '도전적인', '능동적인', '당신을 기다려왔습니다!'];
+  const ref = useRef<HTMLDivElement>(null);
+
+  return (
+    <div
+      ref={ref}
+      className="relative flex h-[982px] w-full items-center justify-center"
+    >
+      <div className="h-full w-full overflow-y-auto">
+        <span className=" font-Pretendard h-full w-full text-[50px] font-[600] leading-[59.67px] tracking-[-1%]">
+          {ideal[0]}
+        </span>
+      </div>
+      <img
+        className="absolute right-0"
+        src={imgData.idealImgData.nodes[0].publicURL}
+        alt={imgData.idealImgData.nodes[0].name}
+      />
+    </div>
+  );
 }
 
 export default Ideal;

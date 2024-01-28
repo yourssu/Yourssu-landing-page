3d63c724713b20955aa8dd4bbf6c6d36aef8b3d6
george <hyunrim03@naver.com>
2024-01-28 18:22:41 +0900

    design: 지원 분야 반응형 컴포넌트 디자인 완료

--- src/containers/select/Supporting/DepartmentSearch.tsx
+++ src/containers/select/Supporting/DepartmentSearch.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { NodeType } from '@/types/hook';
 
 export default function DepartmentSearch({
@@ -9,17 +10,23 @@ export default function DepartmentSearch({
   setSearchText: React.Dispatch<React.SetStateAction<string>>;
 }) {
   const Ref = useRef<HTMLInputElement>(null);
-
+  const windowSize = useMediaQuery({
+    query: `(min-width: 1080px)`,
+  });
   return (
-    <div className="flex h-auto w-[1280px] justify-between rounded-[14px] bg-bluegray3-0 px-[32px] py-[17px]">
+    <div className="flex h-auto w-[1280px] justify-between rounded-[14px] bg-bluegray3-0 px-[32px] py-[17px] xs:w-[200px] sm:w-[200px] md:w-[500px] lg:w-[924px]">
       <input
         ref={Ref}
-        className="font-Pretendard w-[800px] border-none bg-bluegray3-0 text-[24px] font-[600] leading-[28.64px] text-bluegray1-0"
-        placeholder="관심있는 분야에 대해 검색해보세요 ex) 인사, 기획, ..."
+        className="font-Pretendard w-[800px] border-none bg-bluegray3-0 text-[24px] font-[600] leading-[28.64px] text-bluegray1-0 xs:w-[100px] xs:text-[14px] xs:font-[400] xs:leading-[16.71px] sm:w-[100px] sm:text-[14px] sm:font-[400] sm:leading-[16.71px] md:w-[350px] lg:w-[600px]"
+        placeholder={
+          windowSize
+            ? `관심있는 분야에 대해 검색해보세요 ex) 인사, 기획, ...`
+            : '키워드 검색'
+        }
         onChange={() => {
           setTimeout(() => {
             setSearchText(Ref.current!.value);
-          }, 500);
+          }, 2000);
         }}
       />
       <button
@@ -28,7 +35,11 @@ export default function DepartmentSearch({
           setSearchText(Ref.current!.value);
         }}
       >
-        <img className="w-[32px]" src={imgData.publicURL} alt={imgData.name} />
+        <img
+          className="w-[32px] xs:w-[21px] sm:w-[21px]"
+          src={imgData.publicURL}
+          alt={imgData.name}
+        />
       </button>
     </div>
   );
--- src/containers/select/Supporting/index.tsx
+++ src/containers/select/Supporting/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import tw from 'tailwind-styled-components';
 import DepartmentCard from './DepartmentCard';
 import DepartmentSearch from './DepartmentSearch';
@@ -7,6 +8,9 @@ import useSupportingDetail from './hook';
 function Supporting() {
   const { data, imgData } = useSupportingDetail();
   const [searchText, setSearchText] = useState<string>('');
+  const windowSize = useMediaQuery({
+    query: `(min-width: 1080px)`,
+  });
 
   useEffect(() => {
     setSearchText('');
@@ -19,13 +23,20 @@ function Supporting() {
   return (
     <Container>
       <div className="flex flex-col items-center gap-[20px]">
-        <span className="font-Pretendard text-[50px] font-[600] leading-[59.67px] tracking-[-1%]">
+        <span className="font-Pretendard text-[50px] font-[600] leading-[59.67px] tracking-[-1%] xs:text-[32px] xs:leading-[38.19px] sm:text-[32px] sm:leading-[38.19px] ">
           지원 분야
         </span>
-        <p className="font-Pretendard text-[24px] font-[600] leading-[28.64px] text-gray1-0">
-          총 9개의 분야에서 지원자를 모집하고 있어요. 관심 있는 직군의 카드에
-          마우스를 올려보세요!
-        </p>
+        {windowSize ? (
+          <p className="font-Pretendard text-center text-[24px] font-[600] leading-[28.64px] text-gray1-0 xs:text-[14px] xs:leading-[16.71px] sm:text-[14px] sm:leading-[16.71px]">
+            총 9개의 분야에서 지원자를 모집하고 있어요. 관심 있는 직군의 카드에
+            마우스를 올려보세요!
+          </p>
+        ) : (
+          <p className="font-Pretendard text-center text-[24px] font-[600] leading-[28.64px] text-gray1-0 xs:text-[14px] xs:leading-[16.71px] sm:text-[14px] sm:leading-[16.71px]">
+            총 9개의 분야에서 지원자를 모집하고 있어요.
+            <br /> 관심 있는 직군의 카드에 마우스를 올려보세요!
+          </p>
+        )}
       </div>
 
       <div className="flex flex-col items-center gap-[39px]">
@@ -35,14 +46,22 @@ function Supporting() {
         />
         <div
           className={`gap-4 
-            ${filterData.length === 9 ? 'grid grid-cols-10' : 'flex'}
+            ${
+              filterData.length === 9
+                ? 'grid grid-cols-10 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-9'
+                : 'flex flex-col lg:flex-row xl:flex-row xxl:flex-row'
+            }
             `}
         >
           {filterData.map((value, index) => {
             return (
               // eslint-disable-next-line react/jsx-key
               <div
-                className={`col-span-2 ${index === 5 ? 'col-start-2' : null}`}
+                className={`col-span-2 lg:col-span-3 ${
+                  index === 5
+                    ? 'col-start-2 xs:col-start-1 sm:col-start-1 md:col-start-3'
+                    : null
+                }`}
               >
                 <DepartmentCard
                   data={value}
@@ -59,6 +78,10 @@ function Supporting() {
 
 const Container = tw.div`
   w-[1376px]
+  lg:w-[924px]
+  md:w-[500px]
+  sm:w-auto
+  xs:w-auto
   flex
   flex-col
   gap-[60px]

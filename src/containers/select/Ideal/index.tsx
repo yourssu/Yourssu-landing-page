6de76c11a2fef57676dd9ffcc701dd44080078b9
george <hyunrim03@naver.com>
2024-02-04 16:40:52 +0900

    design: Ideal 반응형 컴포넌트 디자인 완료

--- src/containers/select/Ideal/index.tsx
+++ src/containers/select/Ideal/index.tsx
@@ -1,27 +1,34 @@
-import { useRef } from 'react';
 import useIdealDetail from './hook';
 
 function Ideal() {
   const { imgData } = useIdealDetail();
   const ideal = ['자발적인', '도전적인', '능동적인', '당신을 기다려왔습니다!'];
-  const ref = useRef<HTMLDivElement>(null);
 
   return (
-    <div
-      ref={ref}
-      className="relative flex h-[982px] w-full items-center justify-center"
-    >
-      <div className="h-full w-full overflow-y-auto">
-        <span className=" font-Pretendard h-full w-full text-[50px] font-[600] leading-[59.67px] tracking-[-1%]">
-          {ideal[0]}
-        </span>
-      </div>
-      <img
-        className="absolute right-0"
-        src={imgData.idealImgData.nodes[0].publicURL}
-        alt={imgData.idealImgData.nodes[0].name}
-      />
-    </div>
+    <>
+      {ideal.map((value, index) => {
+        return (
+          <div
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            className="relative flex h-[982px] w-full items-center  justify-center xs:h-[542px] sm:h-[542px] md:h-[666px]"
+          >
+            <div className="overflow-y-auto">
+              <span className=" font-Pretendard h-full w-full text-[50px] font-[700] leading-[59.67px] tracking-[-1%] xs:text-[15px] xs:font-[600] xs:leading-[20.19px] sm:text-[15px] sm:font-[600] sm:leading-[20.19px] md:text-[40px] md:leading-[47.74px]">
+                {value}
+              </span>
+            </div>
+            <img
+              className={`absolute ${
+                index % 2 === 0 ? 'right-0' : 'left-0 -scale-x-100 transform'
+              } xs:w-[128px] sm:w-[128px] md:w-[260px]`}
+              src={imgData.idealImgData.nodes[0].publicURL}
+              alt={imgData.idealImgData.nodes[0].name}
+            />
+          </div>
+        );
+      })}
+    </>
   );
 }
 

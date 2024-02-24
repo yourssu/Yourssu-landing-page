8dd4352af35aa6d36cc3ea06c4dbb4b431c5f043
george <hyunrim03@naver.com>
2024-02-24 10:22:41 +0900

    design: recruiting 페이지 컨테이너 넣기

--- src/pages/recruiting/index.tsx
+++ src/pages/recruiting/index.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import Layout from '@/components/Layout';
 import Seo from '@/components/Seo';
+import About from '@/containers/select/About';
+import ApplyProcedure from '@/containers/select/ApplyProcedure';
 import Banner from '@/containers/select/Banner';
+import FAQ from '@/containers/select/FAQ';
+import Ideal from '@/containers/select/Ideal';
+import Supporting from '@/containers/select/Supporting';
 import { OSType } from '@/types/landing.type';
 
 function Recruiting() {
@@ -20,6 +25,11 @@ function Recruiting() {
     <Layout pageType="recruiting" type={type}>
       <div className="flex w-full flex-col items-center justify-center gap-[180px] py-[50px]">
         <Banner />
+        <Ideal />
+        <About />
+        <ApplyProcedure />
+        <Supporting />
+        <FAQ />
       </div>
     </Layout>
   );

26d87084c5b8db43b79b9c33dbb894e9f12d8260
fecapark <feca@daangn.com>
2024-09-01 02:55:58 +0900

    feat: 뿌슝이 랜딩 애니메이션 구현

--- src/containers/select/Ideal/index.tsx
+++ src/containers/select/Ideal/index.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useRef, useState } from 'react';
+import { motion, transform, useScroll } from 'framer-motion';
 import tw from 'tailwind-styled-components';
 import useIdealDetail from './hook';
 
 function Ideal() {
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
   const { imgData } = useIdealDetail();
   const ideal = [
     {
@@ -21,39 +24,109 @@ function Ideal() {
       title: '당신을\n기다려왔습니다!',
     },
   ];
+  const { scrollY } = useScroll();
+  const [scrollBoundingBox, setScrollBoundingBox] = useState({
+    top: 0,
+    bottom: 0,
+  });
+  const [opacityValues, setOpacityValues] = useState<number[]>(
+    ideal.map(() => 0),
+  );
+
+  scrollY.on('change', (value) => {
+    const opacities = ideal.map((_, index) => {
+      const wholeScrollAreaHeight = Math.max(
+        scrollBoundingBox.bottom - scrollBoundingBox.top,
+        0,
+      );
+      const fadeHeightRatio = 0.3;
+      const eachFadeScrollAreaHeight =
+        (wholeScrollAreaHeight / ideal.length) * fadeHeightRatio;
+      const eachScrollAreaHeight = wholeScrollAreaHeight / ideal.length;
+      const fadeInStart = scrollBoundingBox.top + eachScrollAreaHeight * index;
+      const fadeInEnd = fadeInStart + eachFadeScrollAreaHeight;
+      const fadeOutEnd = fadeInStart + eachScrollAreaHeight;
+      const fadeOutStart = fadeOutEnd - eachFadeScrollAreaHeight;
+      return transform(
+        value,
+        [fadeInStart, fadeInEnd, fadeOutStart, fadeOutEnd],
+        [0, 1, 1, 0],
+      );
+    });
+
+    setOpacityValues(opacities);
+  });
+
+  useEffect(() => {
+    if (!scrollContainerRef.current) return;
+
+    const { top, height } = scrollContainerRef.current.getBoundingClientRect();
+
+    setScrollBoundingBox({
+      top,
+      bottom: top + height,
+    });
+  }, []);
 
   return (
     <>
-      {ideal.map((value, index) => {
-        return (
-          <Container
-            key={value.id}
-            className="relative flex items-center justify-center"
-          >
-            <div className="text-center ">
-              <IdealText>{value.title}</IdealText>
+      <ScrollContainer ref={scrollContainerRef}>
+        {ideal.map((value, index) => {
+          return (
+            <div key={value.id} className="sticky top-0">
+              <Container>
+                <Item
+                  className="-translate-y-1/4"
+                  style={{ opacity: opacityValues[index] }}
+                >
+                  <div className="text-center">
+                    <IdealText>{value.title}</IdealText>
+                  </div>
+                  {index !== 3 ? (
+                    <IdealImage
+                      $index={index}
+                      src={imgData.idealImgData.nodes[0].publicURL}
+                      alt={imgData.idealImgData.nodes[0].name}
+                    />
+                  ) : null}
+                </Item>
+              </Container>
             </div>
-            {index !== 3 ? (
-              <IdealImage
-                $index={index}
-                src={imgData.idealImgData.nodes[0].publicURL}
-                alt={imgData.idealImgData.nodes[0].name}
-              />
-            ) : null}
-          </Container>
-        );
-      })}
+          );
+        })}
+      </ScrollContainer>
+      <ScrollAreaBottomSpace />
     </>
   );
 }
 
-const Container = tw.div`
+const ScrollContainer = tw.div`
+  relative 
+  
+  flex 
+  flex-col
+  
   w-full
-  h-[982px]
+  h-[800vh]
+`;
+
+const Container = tw.div`
+  absolute
+
+  w-full
   md:h-[666px]
   sm:h-[542px]
   xs:h-[542px]
 `;
+
+const Item = tw(motion.div)`
+  w-full
+  h-[200vh]
+
+  flex
+  justify-center
+  items-center
+  `;
 
 const IdealText = tw.span`
   text-center
@@ -80,4 +153,9 @@ const IdealImage = tw.img<{ $index: number }>`
   sm:w-[110px]
   xs:w-[100px]
 `;
+
+const ScrollAreaBottomSpace = tw.div`
+  h-[40vh]
+`;
+
 export default Ideal;

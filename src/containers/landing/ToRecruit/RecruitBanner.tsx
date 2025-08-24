8aebcd779fdbdad6cba3565ceb434a957542be54
djhwang-fanmaum <dj.hwang@fanmaum.com>
2025-08-24 14:29:51 +0900

    feat: 지원 배너 구현

---
+++ src/containers/landing/ToRecruit/RecruitBanner.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'gatsby';
+
+interface RecruitmentBannerProps {
+  imageUrl: string;
+  recruitTitle: string;
+}
+
+function RecruitmentBanner({ imageUrl, recruitTitle }: RecruitmentBannerProps) {
+  return (
+    <div
+      className="relative flex h-96 w-full flex-col items-center justify-end gap-9 bg-cover bg-center py-32"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      <div
+        className="absolute inset-0 bg-black-0 bg-opacity-50"
+        aria-hidden="true"
+      />
+
+      <h2 className="relative text-center text-4xl font-semibold leading-[52px] text-white-0">
+        {recruitTitle}
+      </h2>
+
+      <Link
+        to="/recruiting"
+        className="relative flex items-center justify-center rounded-[1rem] bg-[#6B5CFF] px-5 py-3.5 text-base font-semibold leading-normal text-white-0 transition-colors"
+      >
+        지원하러 가기
+      </Link>
+    </div>
+  );
+}
+
+export default RecruitmentBanner;
---
+++ src/containers/landing/ToRecruit/mock.ts
@@ -0,0 +1,10 @@
+interface RecruitData {
+  imageUrl: string;
+  recruitTitle: string;
+}
+
+export const RECRUIT_DATA: RecruitData = {
+  imageUrl:
+    'https://img.freepik.com/premium-photo/group-people-having-meeting_276940-24.jpg',
+  recruitTitle: '새로운 도전을 재미로 바꾸는 사람, 유어슈로 모여요.',
+};

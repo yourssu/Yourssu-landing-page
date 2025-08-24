4f8a32d29dd07f1b821c9da305bd38b51a30cdcc
djhwang-fanmaum <dj.hwang@fanmaum.com>
2025-08-24 14:27:18 +0900

    feat: core value 인터렉션 구현

---
+++ src/containers/landing/CoreValue/mock.ts
@@ -0,0 +1,33 @@
+import value1Des from 'src/assets/images/core-value/core-value-1-description.png';
+import value1 from 'src/assets/images/core-value/core-value-1.png';
+import value2Des from 'src/assets/images/core-value/core-value-2-description.png';
+import value2 from 'src/assets/images/core-value/core-value-2.png';
+import value3Des from 'src/assets/images/core-value/core-value-3-description.png';
+import value3 from 'src/assets/images/core-value/core-value-3.png';
+import value4Des from 'src/assets/images/core-value/core-value-4-description.png';
+import value4 from 'src/assets/images/core-value/core-value-4.png';
+import value5Des from 'src/assets/images/core-value/core-value-5-description.png';
+import value5 from 'src/assets/images/core-value/core-value-5.png';
+
+export const CORE_VALUES = [
+  {
+    coreValue: value1,
+    coreValueDescription: value1Des,
+  },
+  {
+    coreValue: value2,
+    coreValueDescription: value2Des,
+  },
+  {
+    coreValue: value3,
+    coreValueDescription: value3Des,
+  },
+  {
+    coreValue: value4,
+    coreValueDescription: value4Des,
+  },
+  {
+    coreValue: value5,
+    coreValueDescription: value5Des,
+  },
+];

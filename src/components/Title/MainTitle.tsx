b05151e9858205f45f5bc8868c496d2da3e2c0dc
nebulaBdj <114459629+nebulaBdj@users.noreply.github.com>
2025-08-25 20:42:45 +0900

    refactor: 2025-2 리크루팅을 위한 메인 페이지 반응형 적용 (#79)
    * feat: 채널 이미지와 링크 추가
    
    * fix: 메인 페이지 배너 이미지 변경 및 반응형 수정
    
    * fix: 메인 페이지  최하단 배너 이미지 변경 및 반응형 수정
    
    * fix: 잘못된 내용 수정
    
    * fix: 미션과 비전 모바일에서는 이상하게 표시되고, 따로 디자인이 존재하지 않아 모바일에서는 hidden 처리
    
    * feat: 스크롤바 숨기는 스타일 추가
    
    * feat: 텍스트 반응형 추가, 채널 섹션 스크롤 적용
    
    * feat: 반응형 마진과 패딩 적용
    
    * fix: 변경된 핵심 가치 카드 등록

--- src/components/Title/MainTitle.tsx
+++ src/components/Title/MainTitle.tsx
@@ -7,10 +7,12 @@ interface MainTitleProps {
 function MainTitle({ title, subTitle, className = '' }: MainTitleProps) {
   return (
     <div
-      className={`flex w-full flex-col items-start justify-start gap-1 text-center md:text-left ${className}`}
+      className={`flex w-full flex-col items-start justify-start gap-1 text-center ${className}`}
     >
-      <p className="text-2xl font-semibold text-[#4B505D]">{title}</p>
-      <h1 className="text-black text-[2.5rem] font-semibold  leading-[3.25rem]">
+      <p className="text-2xl font-semibold text-[#4B505D] sm:text-base md:text-lg">
+        {title}
+      </p>
+      <h1 className="text-black text-[2.5rem] font-semibold leading-[3.25rem] sm:text-2xl md:text-3xl">
         {subTitle}
       </h1>
     </div>
--- src/containers/landing/Banner/index.tsx
+++ src/containers/landing/Banner/index.tsx
@@ -18,14 +18,14 @@ function Banner() {
         />
       )}
       {/* 390, 720 화면 */}
-      <div className="absolute top-[132px] flex w-full flex-col items-center justify-center xs:top-[75px] lg:hidden xl:hidden xxl:hidden">
-        <span className="font-pretendard font-normal text-white-0 xs:text-[16px] xs:leading-[16px] sm:text-[20px] sm:leading-[20px] md:text-[24px] md:leading-[24px]">
+      <div className="absolute top-[278px] flex w-full flex-col items-center justify-center xs:top-[278px] lg:hidden xl:hidden xxl:hidden">
+        <span className="font-pretendard font-semibold text-white-0 xs:text-[18px] xs:leading-[20px] sm:text-[22px] sm:leading-[20px] md:text-[36px] md:leading-[36px]">
           좋아서 몰입하는 사람들과 함께하세요
         </span>
       </div>
 
       {/* 1080, 1440, 1920 화면 */}
-      <div className="absolute top-[330px] flex justify-center xs:hidden   sm:hidden md:hidden lg:top-[278px] lg:min-w-[870px] xl:min-w-[1160px] xxl:min-w-[1280px]">
+      <div className="absolute top-[330px] flex justify-center xs:hidden sm:hidden md:hidden lg:min-w-[870px] xl:min-w-[1160px] xxl:min-w-[1280px]">
         <span className="font-pretendard font-semibold text-white-0 lg:text-[36px] lg:leading-[36px] xl:text-[46px] xl:leading-[46px] xxl:text-[48px] xxl:leading-[48px]">
           좋아서 몰입하는 사람들과 함께하세요
         </span>

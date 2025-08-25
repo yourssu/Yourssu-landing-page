2e042c24ef2941c89e0b677fda86430e96c8996a
nebulaBdj <dongdung5070@gmail.com>
2025-08-25 11:30:22 +0900

    feat: recruit 페이지 새로운 배너 추가 (임시적으로 이미지 사용)

---
+++ src/containers/select/Banner/new/index.tsx
@@ -0,0 +1,11 @@
+import recruitBannerImage from 'src/assets/images/recruit/banner/recruit-top-banner.png';
+
+function RecruitBanner() {
+  return (
+    <section className="w-full p-5">
+      <img src={recruitBannerImage} alt="리크루팅 배너 이미지 2025-2" />
+    </section>
+  );
+}
+
+export default RecruitBanner;

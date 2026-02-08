69306b92b92c039343796286eb7e97dda19f6a24
이나영 <112078717+wwwlnyy@users.noreply.github.com>
2026-02-08 14:45:55 +0900

    Revert "feat: MainTitle 컴포넌트 수정"
    This reverts commit 695476b440f84fc269928cc92473261eb69cb3e7.

--- src/components/Title/MainTitle.tsx
+++ src/components/Title/MainTitle.tsx
@@ -1,19 +1,20 @@
 interface MainTitleProps {
   title: string;
   subTitle: string;
+  className?: string;
 }
 
-function MainTitle({ title, subTitle }: MainTitleProps) {
+function MainTitle({ title, subTitle, className = '' }: MainTitleProps) {
   return (
-    <div className="mx-auto mb-8 flex w-full max-w-[1060px] flex-col items-start gap-[4px]">
-      <span className="font-pretendard text-[14px] font-normal leading-[20px] tracking-[-0.28px] text-text-basicTertiary">
+    <div
+      className={`flex w-full flex-col items-start justify-start gap-1 text-center ${className}`}
+    >
+      <p className="text-2xl font-semibold text-[#4B505D] sm:text-base md:text-lg">
         {title}
-      </span>
-      <div className="flex w-full items-center justify-between">
-        <span className="font-pretendard text-[24px] font-semibold leading-[38px] tracking-[-0.48px]">
-          {subTitle}
-        </span>
-      </div>
+      </p>
+      <h1 className="text-black text-[2.5rem] font-semibold leading-[3.25rem] sm:text-2xl md:text-3xl">
+        {subTitle}
+      </h1>
     </div>
   );
 }

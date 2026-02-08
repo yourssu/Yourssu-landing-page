c6bf02ead57fdb7be894fbd47feec99867e6c4b1
wwwlnyy <wwwlnyy@gmail.com>
2026-02-08 14:37:06 +0900

    feat: Banner 섹션 모바일 뷰 적용

--- src/components/Button/BoxButton.tsx
+++ src/components/Button/BoxButton.tsx
@@ -7,7 +7,7 @@ interface BoxButtonProps {
 export function BoxButton({ children, width, onClick }: BoxButtonProps) {
   return (
     <div
-      className="B1_Sb_16 inline-flex h-[52px] cursor-pointer items-center justify-center gap-1 self-stretch rounded-[16px] bg-bg-brandPrimary px-[20px] text-text-basicWhite"
+      className="B1_Sb_16 xs:B3_Sb_14 sm:B3_Sb_14 inline-flex h-[52px] cursor-pointer items-center justify-center gap-1 self-stretch rounded-[16px] bg-bg-brandPrimary px-[20px] text-text-basicWhite"
       style={{ width }}
       onClick={onClick}
     >

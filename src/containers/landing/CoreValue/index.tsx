e6ba7990be8db88a298a4d0d5de1bc100547e096
이나영 <112078717+wwwlnyy@users.noreply.github.com>
2026-02-08 14:45:55 +0900

    Revert "fix: CoreValue에서 호버 효과 바로 안 나타나는 문제 수정"
    This reverts commit 36e8b8aa6090e74e8576e3f8f489576dd2832a76.

--- src/containers/landing/CoreValue/index.tsx
+++ src/containers/landing/CoreValue/index.tsx
@@ -10,11 +10,11 @@ function CoreValue() {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   return (
-    <section className="relative z-0 mx-auto flex w-full max-w-[1060px] flex-col items-center justify-center py-20 xs:px-5 sm:px-5 md:px-5">
+    <section className="relative mx-auto flex w-full max-w-[65.625rem] flex-col items-center justify-center py-20 xs:px-5 sm:px-5 md:px-5">
       <AnimatePresence>
         {hoveredIndex !== null && (
           <motion.div
-            className="pointer-events-none fixed inset-0 z-10 bg-black-0 bg-opacity-50"
+            className="fixed inset-0 z-10 bg-black-0 bg-opacity-50"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
@@ -23,13 +23,10 @@ function CoreValue() {
         )}
       </AnimatePresence>
 
-      <MainTitle
-        title="Core Value"
-        subTitle="유어슈가 중요하게 생각하는 것들"
-      />
+      <MainTitle title="Core Value" subTitle="유어슈의 핵심 가치" />
 
       <div
-        className="relative flex flex-wrap justify-center gap-4"
+        className="flex flex-wrap justify-center gap-4"
         onMouseLeave={() => setHoveredIndex(null)}
       >
         {CORE_VALUES.map((value, index) => (

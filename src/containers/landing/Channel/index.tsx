84de42870da6348f003f7869c12966f9021dc8fd
이나영 <112078717+wwwlnyy@users.noreply.github.com>
2026-02-08 14:45:55 +0900

    Revert "feat: Channel 섹션 수정"
    This reverts commit 3a2ebcfd708d94cda0b0f639a6f9901b9c7c77d3.

--- src/containers/landing/Channel/index.tsx
+++ src/containers/landing/Channel/index.tsx
@@ -1,10 +1,9 @@
-import { ContentsCard } from './ContentsCard';
 import { CHANNELS_DATA } from './mock';
 
 function Channel() {
   return (
     <section className="mb-7 h-[30.5rem] py-[3.75rem] text-center">
-      <h3 className="mb-[2.375rem] font-pretendard text-[1.75rem] font-semibold xs:text-2xl sm:text-2xl md:text-2xl">
+      <h3 className="mb-[2.375rem] text-[1.75rem] font-semibold xs:text-2xl sm:text-2xl md:text-2xl">
         다양한 채널에서 유어슈를 만나보세요
       </h3>
 
@@ -14,13 +13,28 @@ function Channel() {
         id="scrollbar-hide"
       >
         {CHANNELS_DATA.map((channel) => (
-          <ContentsCard
-            key={channel.title}
-            title={channel.title}
-            tagNames={channel.tagNames}
-            imageUrl={channel.imageUrl}
-            contentUrl={channel.link}
-          />
+          <a
+            key={channel.platform}
+            href={channel.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex h-full flex-col items-center justify-center gap-3"
+          >
+            <img
+              className="h-48 w-72 rounded-[1rem] bg-zinc-300"
+              src={channel.imageUrl}
+              alt={channel.title}
+            />
+
+            <div className="w-72 px-1 text-left">
+              <p className="text-sm font-normal text-gray-700">
+                {channel.platform}
+              </p>
+              <h4 className="text-black whitespace-pre-wrap text-xl font-semibold">
+                {channel.title}
+              </h4>
+            </div>
+          </a>
         ))}
       </div>
     </section>

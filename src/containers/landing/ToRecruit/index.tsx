91408d1c016744a5d3f0a11e169e723231d69038
wwwlnyy <wwwlnyy@gmail.com>
2026-02-15 00:57:50 +0900

    feat: 하단 RecuritBanner 모바일 뷰 적용 및 BoxButton 컴포넌트 수정

--- src/containers/landing/ToRecruit/index.tsx
+++ src/containers/landing/ToRecruit/index.tsx
@@ -3,10 +3,11 @@ import { RECRUIT_DATA } from './mock';
 
 function ToRecruit() {
   return (
-    <section>
+    <section className="-mx-5 w-[calc(100%+40px)] xs:pb-10 sm:pb-10">
       <RecruitmentBanner
         imageUrl={RECRUIT_DATA.imageUrl}
         recruitTitle={RECRUIT_DATA.recruitTitle}
+        buttonText={RECRUIT_DATA.buttonText}
       />
     </section>
   );

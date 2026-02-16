4311dcbb2d6a1dd151f72a35b3f9e693e7d4ba31
wwwlnyy <wwwlnyy@gmail.com>
2026-02-16 11:09:38 +0900

    feat: Supporting 섹션 수정

--- src/components/Layout/RecruitSectionLayout.tsx
+++ src/components/Layout/RecruitSectionLayout.tsx
@@ -4,7 +4,7 @@ interface RecruitLayoutProps {
 
 function RecruitSectionLayout({ children }: RecruitLayoutProps) {
   return (
-    <section className="flex w-full max-w-[75rem] flex-col gap-10 py-20 xs:px-5 sm:px-5 md:px-5">
+    <section className="flex w-full flex-col gap-10 px-[120px] py-20 xs:gap-6 xs:px-0 xs:py-10 sm:gap-6 sm:px-0 sm:py-10">
       {children}
     </section>
   );

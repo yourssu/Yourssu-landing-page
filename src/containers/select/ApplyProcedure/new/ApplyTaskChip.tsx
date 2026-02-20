9720c042f039711a316bb9f9fe34609437db389a
wwwlnyy <wwwlnyy@gmail.com>
2026-02-20 14:55:31 +0900

    fix: recruiting > 합류 여정 카드에서 줄바꿈 수정

--- src/containers/select/ApplyProcedure/new/ApplyTaskChip.tsx
+++ src/containers/select/ApplyProcedure/new/ApplyTaskChip.tsx
@@ -7,7 +7,7 @@ function ApplyTaskChip({ SVGIconComponent, chipTitle }: ApplyTaskChipProps) {
   return (
     <div className="flex w-full items-center justify-center gap-2 rounded-[0.75rem] bg-bg-brandSecondary px-5 py-4 xs:px-4 sm:px-4 md:px-4">
       <span>{SVGIconComponent}</span>
-      <p className="B1_Rg_16 xs:B4_Rg_13 sm:B4_Rg_13 md:B4_Rg_13 text-text-brandPrimary">
+      <p className="B1_Rg_16 xs:B4_Rg_13 sm:B4_Rg_13 md:B4_Rg_13 break-words break-keep text-text-brandPrimary">
         {chipTitle}
       </p>
     </div>
